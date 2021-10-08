2af6be6d403a16d4b89f87ba8fa9684477b815d1
Vincent MARION <vmarion1969@gmail.com>
2021-10-08 16:40:06 +0200

    Ajout page profil

--- src/Controller/MainController.php
+++ src/Controller/MainController.php
@@ -2,6 +2,7 @@
 
 namespace App\Controller;
 
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Security;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -16,6 +17,18 @@ class MainController extends AbstractController
     public function home(): Response
     {
         return $this->render('main/home.html.twig');
+    }
+
+    /**
+     * Page de profil
+     *
+     * @Route("/mon-profil/", name="main_profil")
+     * @Security("is_granted('ROLE_USER')")
+     */
+    public function profil(): Response
+    {
+
+        return $this->render('main/profil.html.twig');
     }
 
 }

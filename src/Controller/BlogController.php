833e52f2f34829fc5c86b205f1ce80ba3c7d9198
Anthony <a@a.a>
2021-10-12 16:10:11 +0200

    Ajout page résultat de recherche

--- src/Controller/BlogController.php
+++ src/Controller/BlogController.php
@@ -108,4 +108,43 @@ class BlogController extends AbstractController
         ]);
     }
 
+    /**
+     * @Route("/recherche/", name="search")
+     */
+    public function search(Request $request, PaginatorInterface $paginator): Response
+    {
+
+        // Récupération du numéro de la page demandée dans l'URL
+        $requestedPage = $request->query->getInt('page', 1);
+
+        // Vérification que le numéro est positif
+        if($requestedPage < 1){
+            throw new NotFoundHttpException();
+        }
+
+        $search = $request->query->get('q', '');
+
+        $em = $this->getDoctrine()->getManager();
+
+        $query = $em
+            ->createQuery('SELECT a FROM App\Entity\Article a WHERE a.title LIKE :search OR a.content LIKE :search ORDER BY a.publicationDate DESC')
+            ->setParameters([
+                'search' => '%' . $search . '%',
+            ])
+        ;
+
+        // Récupération des articles
+        $articles = $paginator->paginate(
+            $query,
+            $requestedPage,
+            10
+        );
+
+
+        return $this->render('blog/listSearch.html.twig', [
+            'articles' => $articles,
+        ]);
+
+    }
+
 }
